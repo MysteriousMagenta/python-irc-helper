51f0800d50fbd77b91437d91c741f549b448ee58
MysteriousMagenta <therealrebuilder@gmail.com>
2015-04-30 21:12:44 +0200

    Renamed variables to allow for other databases/cursors.

--- irc_helper/main_bot.py
+++ irc_helper/main_bot.py
@@ -18,12 +18,12 @@ class IRCHelper(IRCBot):
     def __init__(self, database_name, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.commands = []
-        self.database = sqlite3.connect(database_name)
-        self.cursor = self.database.cursor()
-        self.cursor.execute("SELECT name FROM sqlite_master WHERE type=\"table\"")
-        tables = tuple(map(lambda x: x[0], self.cursor.fetchall()))
+        self.command_database = sqlite3.connect(database_name)
+        self.command_cursor = self.command_database.cursor()
+        self.command_cursor.execute("SELECT name FROM sqlite_master WHERE type=\"table\"")
+        tables = tuple(map(lambda x: x[0], self.command_cursor.fetchall()))
         if "Commands" not in tables:
-            self.cursor.execute("CREATE TABLE Commands (id INTEGER PRIMARY KEY, trigger TEXT, response TEXT)")
+            self.command_cursor.execute("CREATE TABLE Commands (id INTEGER PRIMARY KEY, trigger TEXT, response TEXT)")
 
     # To add a command.
     # For commands that are functions.
@@ -34,11 +34,11 @@ class IRCHelper(IRCBot):
     def basic_command(self, *args, **kwargs):
         def basic_decorator(command):
             trigger, response = command(*args, **kwargs)
-            self.cursor.execute("SELECT * FROM Commands")
-            if self.cursor.fetchone() is None:
-                self.cursor.execute("INSERT INTO Commands VALUES (0,?,?)", (trigger, response))
+            self.command_cursor.execute("SELECT * FROM Commands")
+            if self.command_cursor.fetchone() is None:
+                self.command_cursor.execute("INSERT INTO Commands VALUES (0,?,?)", (trigger, response))
             else:
-                self.cursor.execute("INSERT INTO Commands(trigger,response) VALUES (?,?)", (trigger, response))
+                self.command_cursor.execute("INSERT INTO Commands(trigger,response) VALUES (?,?)", (trigger, response))
             return command
         return basic_decorator
 
@@ -48,8 +48,8 @@ class IRCHelper(IRCBot):
             for func_command in self.commands:
                 if func_command(self, block_data.get("message"), block_data.get("sender")) is not None:
                     break
-            self.cursor.execute("SELECT trigger,response FROM Commands")
-            for trigger, response in self.cursor.fetchall():
+            self.command_cursor.execute("SELECT trigger,response FROM Commands")
+            for trigger, response in self.command_cursor.fetchall():
                 matched = re.match(trigger, block_data.get("message", ""))
                 if matched:
                     self.send_message(response)
@@ -58,4 +58,4 @@ class IRCHelper(IRCBot):
     def quit(self):
         self.leave_channel()
         self.socket.close()
-        self.database.close()
+        self.command_database.close()
