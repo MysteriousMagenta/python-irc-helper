029abbc39cbf7c9fbfe74f99e1cc127679f3ca13
MysteriousMagenta <therealrebuilder@gmail.com>
2015-04-30 18:55:02 +0200

    Just look at the code.

--- irc_helper/__main__.py
+++ irc_helper/__main__.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-from irc_helper import IRCBot
+# A Classic Example of "Why does this work?!"
+import os
+import sys
+parent_directory = os.sep.join(os.path.abspath(__file__).split(os.sep)[:-2])
+if parent_directory not in sys.path:
+    sys.path.insert(0, parent_directory)
+
+import irc_helper
 import json
+
 
 needed = ("nick", "user", "channel", "port", "host")
 with open("config.json") as config_file:
     config = json.loads(config_file.read())
 
-IRCBot(**{x:y for x, y in config.items() if x in needed}).run()
+irc_helper.IRCBot(**{x:y for x, y in config.items() if x in needed}).run()
