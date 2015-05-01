983b3c9e33117a28d3765718be024b9af2b1239e
MysteriousMagenta <therealrebuilder@gmail.com>
2015-05-01 23:30:59 +0200

    Started working on side-thing.

--- irc_helper/__init__.py
+++ irc_helper/__init__.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 from irc_helper.irc_protocol import IRCBot, IRCError
 from irc_helper.main_bot import IRCHelper, FLAGS
+try:
+    from irc_helper.tooth_bot import Toothless
+except ImportError:
+    pass
