5e12bc6094482192939e7e0040d664eec57fdde7
ColorfulBanana <therealrebuilder@gmail.com>
2015-05-09 00:24:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
-import setuptools
+import os.path
+from setuptools import setup, find_packages
+
+if os.path.exists("requirements.txt"):
+    with open("requirements.txt") as req_file:
+        reqs = [i for i in req_file if i]
+else:
+    reqs = []
+
+packs = find_packages()
+if not packs:
+    raise Exception("Couldn't find any packages!")
 
 
-scripts = [
-    "irc_helper/irc_helper.py",
-    "irc_helper/irc_protocol.py"
-]
-
-setuptools.setup(
+setup(
     name="irc_helper",
-    version="1.0",
-    packages=packs,
-    scripts=scripts,
+    version=1.0,
+    description="A module that helps with IRC, namely IRC Bots.",
+    author="SquishyStrawberry",
     install_requires=reqs,
-    description="Small Lib that helps with IRC, namely IRC Bots.",
+    packages=packs
 )
